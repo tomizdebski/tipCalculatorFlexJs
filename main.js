44fba14cd453c578c075fb12ce8f43e8e9d844ff
Tomasz Izdebski <lukas4096@gmail.com>
2023-05-16 21:32:19 +0200

    cdd

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-let bill = document.querySelector('.calculator__input-bill').value;
-let numOfPeople = document.querySelector('.calculator__num-people-input').value;
-const buttonPercent = document.querySelectorAll('.calculator__input-tip-btn');
-const tipAmount = document.querySelector('.calculator__result-amount');
-const total = document.querySelector('.calculator__result-total');
-const reset = document.querySelector('.calculator__button-reset');
+let bill = document.querySelector(".calculator__input-bill").value;
+let numOfPeople = document.querySelector(
+  ".calculator__num-people-input"
+).value;
+const buttonPercent = document.querySelectorAll(".calculator__input-tip-btn");
+const tipAmount = document.querySelector(".calculator__result-amount");
+const total = document.querySelector(".calculator__result-total");
+const reset = document.querySelector(".calculator__button-reset");
 
-buttonPercent.forEach((el) => el.addEventListener('click', (e) => {
-    const tipPercent = e.target.value;
+buttonPercent.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    let tipPercent = e.target.value;
     console.log(bill, numOfPeople, tipPercent);
 
-    tipAmount.innerText = '$' + ((+bill * (+(tipPercent.slice(0, -1)) / 100)) / +numOfPeople) ;
-    total.innerText = '$' + ((+bill + (+bill * (+(tipPercent.slice(0, -1)) / 100))) / +numOfPeople);
+    bill = document.querySelector(".calculator__input-bill").value;
+    numOfPeople = document.querySelector(".calculator__num-people-input").value;
+    if( !isNaN(+tipPercent)) tipPercent+= "%";
 
-}));
+    tipAmount.innerText =
+      "$" + (+bill * (+tipPercent.slice(0, -1) / 100)) / +numOfPeople;
+    total.innerText =
+      "$" + (+bill + +bill * (+tipPercent.slice(0, -1) / 100)) / +numOfPeople;
+  })
+);
 
-reset.addEventListener('click', (e) => {
-    tipAmount.innerText = "$0.00";
-    total.innerText = "$0.00";
-    bill = "";
-    numOfPeople = "";
-    
-})
+reset.addEventListener("click", (e) => {
+  tipAmount.innerText = "$0.00";
+  total.innerText = "$0.00";
 
+  document.querySelector(".calculator__input-bill").value = "";
+  document.querySelector(".calculator__num-people-input").value = "";
+  document.querySelector(".itb").value = "";
 
-
-
-
+});
